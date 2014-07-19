7b5bb3664decff4f2a49350e3c3713c7f5b3292f
Michael Koltashew <snake_kmm@mail.ru>
2014-07-19 23:22:19 +0300

    Added map function

--- JavascriptSecondTask/Content/Scripts/Functions.js
+++ JavascriptSecondTask/Content/Scripts/Functions.js
@@ -1,8 +1,8 @@
 ﻿(function (functionApplication) {
 
     'use strict';
-
-    functionApplication.curry = function (functionToBeCurried) {
+    var self = functionApplication;
+    self.curry = function (functionToBeCurried) {
 
         var numberOfExplicitParameters = functionToBeCurried.length;
 
@@ -22,16 +22,16 @@
         return getCurriedFunction([]);
     };
 
-    functionApplication.sum = function sum(argument1, argument2, argument3, argument4) {
+    self.sum = function sum(argument1, argument2, argument3, argument4) {
         var totalSum = argument1 + argument2 + argument3 + argument4;
         return totalSum;
     };
 
-    functionApplication.curriedSum = functionApplication.curry(functionApplication.sum);
-    var curryResult = functionApplication.curriedSum(1)(3)(5)(2);
+    self.curriedSum = self.curry(self.sum);
+    var curryResult = self.curriedSum(1)(3)(5)(2);
     console.log('functionApplication.curriedSum(1)(3)(5)(2) = ' + curryResult);
 
-    functionApplication.partial = function (handler) {
+    self.partial = function (handler) {
         var slice = Array.prototype.slice;
         var highOrderArguments = slice.call(arguments, 1);
 
@@ -42,7 +42,7 @@
         };
     };
 
-    functionApplication.sumAll = function sumAll() {
+    self.sumAll = function sumAll() {
         var totalSum = 0;
         var i;
         for (i = 0; i < arguments.length; i += 1) {
@@ -51,9 +51,36 @@
         return totalSum;
     };
 
-    functionApplication.partialSum = functionApplication.partial(functionApplication.sumAll, 1, 2, 3);
-    var partialResult = functionApplication.partialSum(4, 5);
+    self.partialSum = self.partial(self.sumAll, 1, 2, 3);
+    var partialResult = self.partialSum(4, 5);
     console.log('functionApplication.partialSum(4, 5) = ' +
         'functionApplication.partial(functionApplication.sumAll, 1, 2, 3)(4, 5) = ' + partialResult);
+
+
+    self.first = function (array) {
+        return array[0];
+    };
+
+    self.rest = function (array) {
+        return array.slice(1);
+    };
+
+    self.isEmpty = function (array) {
+        return array.length === 0;
+    };
+
+    self.map = function (array, handler) {
+        if (self.isEmpty(array)) {
+            return array;
+        }
+        return [handler(self.first(array))].concat(self.rest(array).map(handler));
+    };
+
+    function square(i) {
+        return i * i;
+    }
+
+    var mapResult = self.map([1, 2, 3, 4, 5], square);
+    console.log('functionApplication.map([1, 2, 3, 4, 5], square) = ' + mapResult);
 
 }(window.functionApplication = window.functionApplication || {}));
